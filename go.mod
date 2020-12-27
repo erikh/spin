6d79512eec406c07703a5620597654a6e6a3ec63
Erik Hollensbe <github@hollensbe.org>
2020-12-27 12:34:00 -0800

    host path spin agent
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- go.mod
+++ go.mod
@@ -3,11 +3,10 @@ module code.hollensbe.org/erikh/spin
 go 1.15
 
 require (
-	github.com/dimfeld/httptreemux v5.0.1+incompatible // indirect
 	github.com/google/uuid v1.1.2
 	github.com/urfave/cli/v2 v2.3.0
 	go.etcd.io/bbolt v1.3.5
 	goa.design/goa v2.2.5+incompatible
 	goa.design/goa/v3 v3.2.6
-	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
+	golang.org/x/sys v0.0.0-20201223074533-0d417f636930
 )
--- pkg/resources/storage/host_path.go
+++ pkg/resources/storage/host_path.go
@@ -6,63 +6,108 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	brokerclient "code.hollensbe.org/erikh/spin/clients/broker"
 	"code.hollensbe.org/erikh/spin/pkg/agent"
 	"code.hollensbe.org/erikh/spin/pkg/agent/dispatcher"
 )
 
-var hostPathDispatcher = dispatcher.Dispatcher{
-	"add_volume": {
-		RequiredParameters: []string{"path"},
-		Dispatch: func(c dispatcher.Command) error {
-			if fi, err := os.Stat(c.Parameters["path"]); err != nil {
-				return err
-			} else if !fi.IsDir() {
-				return errors.New("path is not a directory")
-			}
+func hostPathDispatcher(basePath string) dispatcher.Dispatcher {
+	bp := func(strs ...string) (string, error) {
+		if len(strs) == 0 {
+			return "", errors.New("empty path")
+		}
 
-			return nil
-		},
-	},
-	"remove_volume": {
-		RequiredParameters: []string{"path"},
-		Dispatch: func(c dispatcher.Command) error {
-			if fi, err := os.Stat(c.Parameters["path"]); err != nil {
-				return err
-			} else if !fi.IsDir() {
-				return errors.New("path is not a directory")
-			}
+		strs = append([]string{basePath}, strs...)
 
-			return os.RemoveAll(c.Parameters["path"])
+		full := filepath.Join(strs...)
+		rel, err := filepath.Rel(basePath, full)
+		if err != nil {
+			return "", err
+		}
+
+		if filepath.Clean(rel) == filepath.Clean(basePath) || strings.Contains(rel, "..") {
+			return "", errors.New("path falls below root")
+		}
+
+		return full, nil
+	}
+
+	return dispatcher.Dispatcher{
+		"add_volume": {
+			RequiredParameters: []string{"path"},
+			Dispatch: func(c dispatcher.Command) error {
+				path, err := bp(c.Parameters["path"])
+				if err != nil {
+					return err
+				}
+
+				if fi, err := os.Stat(path); os.IsNotExist(err) {
+					return os.MkdirAll(path, 0700)
+				} else if err != nil {
+					return err
+				} else if !fi.IsDir() {
+					return errors.New("path is not a directory")
+				}
+
+				return errors.New("already exists")
+			},
 		},
-	},
-	"create_image": {
-		RequiredParameters: []string{"volume_path", "image_name", "image_size"},
-		Dispatch: func(c dispatcher.Command) error {
-			return exec.Command("truncate", "-s", fmt.Sprintf("%sG", c.Parameters["image_size"]), filepath.Join(c.Parameters["volume_path"], c.Parameters["image_name"])).Run()
+		"remove_volume": {
+			RequiredParameters: []string{"path"},
+			Dispatch: func(c dispatcher.Command) error {
+				path, err := bp(c.Parameters["path"])
+				if err != nil {
+					return err
+				}
+
+				if fi, err := os.Stat(path); err != nil {
+					return err
+				} else if !fi.IsDir() {
+					return errors.New("path is not a directory")
+				}
+
+				return os.RemoveAll(path)
+			},
 		},
-	},
-	"delete_image": {
-		RequiredParameters: []string{"volume_path", "image_name"},
-		Dispatch: func(c dispatcher.Command) error {
-			return os.Remove(filepath.Join(c.Parameters["volume_path"], c.Parameters["image_name"]))
+		"create_image": {
+			RequiredParameters: []string{"volume_path", "image_name", "image_size"},
+			Dispatch: func(c dispatcher.Command) error {
+				path, err := bp(c.Parameters["volume_path"], c.Parameters["image_name"])
+				if err != nil {
+					return err
+				}
+
+				return exec.Command("qemu-img", "create", "-f", "raw", path, fmt.Sprintf("%sG", c.Parameters["image_size"])).Run()
+			},
 		},
-	},
-	"resize_image": {
-		RequiredParameters: []string{"volume_path", "image_name", "image_size"},
-		Dispatch: func(c dispatcher.Command) error {
-			return nil
+		"delete_image": {
+			RequiredParameters: []string{"volume_path", "image_name"},
+			Dispatch: func(c dispatcher.Command) error {
+				path, err := bp(c.Parameters["volume_path"], c.Parameters["image_name"])
+				if err != nil {
+					return err
+				}
+
+				return os.Remove(path)
+			},
 		},
-	},
-	"move_image": {
-		RequiredParameters: []string{"image_name", "volume", "target_volume"},
-		Dispatch: func(c dispatcher.Command) error {
-			return nil
+		"resize_image": {
+			RequiredParameters: []string{"volume_path", "image_name", "image_size"},
+			Dispatch: func(c dispatcher.Command) error {
+				return nil
+			},
 		},
-	},
+		"move_image": {
+			RequiredParameters: []string{"image_name", "volume", "target_volume"},
+			Dispatch: func(c dispatcher.Command) error {
+				return nil
+			},
+		},
+	}
 }
 
 func NewHostPathAgent(cc brokerclient.Config) *agent.Agent {
-	return agent.New(cc, ResourceType, hostPathDispatcher)
+	return agent.New(cc, ResourceType, hostPathDispatcher("/tmp/host-path-test"))
 }
