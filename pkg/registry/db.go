ac85d007110400238b39388a021f1fafad9b0e87
Erik Hollensbe <github@hollensbe.org>
2020-12-29 08:24:44 -0800

    More refactors around registry design
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- pkg/registry/db.go
+++ pkg/registry/db.go
@@ -1,6 +1,8 @@
 package registry
 
 import (
+	"errors"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -28,4 +30,24 @@ func NewDB(c DBConfig) (*DB, error) {
 	}
 
 	return &DB{db: db}, nil
+}
+
+func (db *DB) Get(id uint64) (*VM, error) {
+	return nil, errors.New("unimplemented")
+}
+
+func (db *DB) Create(vm *VM) (uint64, error) {
+	return 0, errors.New("unimplemented")
+}
+
+func (db *DB) Delete(id uint64) error {
+	return errors.New("unimplemented")
+}
+
+func (db *DB) Update(id uint64, vm *VM) error {
+	return errors.New("unimplemented")
+}
+
+func (db *DB) List() ([]uint64, error) {
+	return nil, errors.New("unimplemented")
 }
