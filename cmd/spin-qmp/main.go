d1006bf509b8e36d3b054c3d5c91a844a9bffc67
Erik Hollensbe <github@hollensbe.org>
2021-01-02 17:39:45 -0800

    Divide raw command interface for QMP tool
    now has a shutdown command too!
    
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- cmd/spin-qmp/main.go
+++ cmd/spin-qmp/main.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"path"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
@@ -16,10 +17,28 @@ func main() {
 	app := cli.NewApp()
 
 	app.Usage = "Send QMP commands to unix socket monitors."
-	app.Description = "Accepts JSON commands over STDIN, yields response over STDOUT."
-	app.UsageText = path.Base(os.Args[0]) + " [monitor socket]"
 
-	app.Action = start
+	app.Commands = []*cli.Command{
+		{
+			Name:        "raw",
+			Usage:       "Accepts JSON commands over STDIN, yields response over STDOUT.",
+			Description: "Accepts JSON commands over STDIN, yields response over STDOUT.",
+			UsageText:   path.Base(os.Args[0]) + " [monitor socket]",
+			Action:      raw,
+		},
+		{
+			Name: "shutdown",
+			Flags: []cli.Flag{
+				&cli.DurationFlag{
+					Name:    "wait",
+					Aliases: []string{"w"},
+					Usage:   "How long to wait before giving up",
+					Value:   10 * time.Second,
+				},
+			},
+			Action: shutdown,
+		},
+	}
 
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -27,34 +46,73 @@ func main() {
 	}
 }
 
-func start(ctx *cli.Context) error {
+func getConn(socket string) (net.Conn, error) {
+	conn, err := net.Dial("unix", socket)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.NewEncoder(conn).Encode(map[string]interface{}{"execute": "qmp_capabilities"}); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	obj := map[string]interface{}{}
+	if err := json.NewDecoder(conn).Decode(&obj); err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func shutdown(ctx *cli.Context) error {
 	if ctx.Args().Len() != 1 {
 		return errors.New("please provide the monitor socket (see --help)")
 	}
 
-	socket := ctx.Args().First()
-
-	conn, err := net.Dial("unix", socket)
+	conn, err := getConn(ctx.Args().First())
 	if err != nil {
 		return err
 	}
 	defer conn.Close()
 
-	obj := map[string]interface{}{}
-
-	if err := json.NewEncoder(conn).Encode(map[string]interface{}{"execute": "qmp_capabilities"}); err != nil {
+	if err := json.NewEncoder(conn).Encode(map[string]string{"execute": "system_powerdown"}); err != nil {
 		return err
 	}
 
-	if err := json.NewDecoder(conn).Decode(&obj); err != nil {
+	go func() {
+		time.Sleep(ctx.Duration("wait"))
+		conn.Close()
+	}()
+
+	for {
+		obj := map[string]interface{}{}
+		if err := json.NewDecoder(conn).Decode(&obj); err != nil {
+			return err
+		}
+		fmt.Println(obj)
+		if event, ok := obj["event"]; ok && event == "SHUTDOWN" {
+			return nil
+		}
+	}
+}
+
+func raw(ctx *cli.Context) error {
+	if ctx.Args().Len() != 1 {
+		return errors.New("please provide the monitor socket (see --help)")
+	}
+
+	conn, err := getConn(ctx.Args().First())
+	if err != nil {
 		return err
 	}
+	defer conn.Close()
 
 	if _, err := io.Copy(conn, os.Stdin); err != nil {
 		return err
 	}
 
-	conn.Write([]byte("\n"))
 	_, err = io.Copy(os.Stdout, conn)
 	return err
 }
