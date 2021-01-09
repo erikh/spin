d3cce603e8af779246c6b0f747cfb5f12631176b
Erik Hollensbe <github@hollensbe.org>
2021-01-09 00:18:44 -0800

    fix for ports < 100
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 import RFB from "@novnc/novnc";
+import sprintf from 'sprintf';
 
 class App extends React.Component {
   componentDidMount() {
-    var id = new URLSearchParams(window.location.search).get("id");
+    var id = parseInt(new URLSearchParams(window.location.search).get("id"));
 
     var rfb = new RFB(
       document.getElementById("root"),
-      `ws://127.0.0.1:60${id}`
+      sprintf("ws://127.0.0.1:60%02d", id)
     );
     rfb.resizeSession = true;
     rfb.scaleViewport = true;
